67d50200b6d2f6896df3ab3b3beeab8221eae91c
Abhishek Kange <84184526+Abhishekkange@users.noreply.github.com>
2025-08-01 02:12:56 +0530

    Animaton module added

---
+++ Cortnonix-ai/include/animation.h
@@ -0,0 +1,13 @@
+#ifndef ANIMATIONS_H
+#define ANIMATIONS_H
+#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h> 
+
+
+void openEyes(Adafruit_SSD1306& display, int delayMs = 50);
+
+
+
+
+#endif
